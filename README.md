2d20222eb074799f800b6e8f4cd23ef3edef5480
Alexander Lazarchuk <olazarchuk@markettraders.com>
2022-04-08 17:31:14 +0300

    #master

---
+++ README.md
@@ -0,0 +1,12 @@
+Example project which shows how to create a WebSocket server using Spring Boot.
+==============================================================================
+
+* `Tutorial`: https://javascopes.com/creating-a-websocket-server-with-spring-boot-424o-9846dd7a
+* `Repo`: https://github.com/nexmo-community/websocket-spring-boot
+
+![Screenshot-1](screenshot_01.png)
+![Screenshot-2](screenshot_02.png)
+
+Server is set to listen on [ws://localhost:8080/socket](ws://localhost:8080/socket) and will respond to both binary and text messages by echoing the contents back to the sender.
+
+Clone the project and run `gradle bootRun` to start it.
---
+++ src/main/java/com/websocketdemo/WebSocketHandler.java
@@ -0,0 +1,27 @@
+package com.websocketdemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+import java.io.IOException;
+import java.util.Date;
+
+@Slf4j
+public class WebSocketHandler extends AbstractWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        String payload = message.getPayload();
+        log.info("(Text) Message Payload Received: '{}'", payload);
+        TextMessage msg = new TextMessage("Response: txt='" + payload + "' date='" + new Date() + "'");
+        session.sendMessage(msg);
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws IOException {
+        log.info("(Binary) Message Payload Received: ", message.getPayload());
+        session.sendMessage(message);
+    }
+}
